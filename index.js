530386f9d53dca612c624e083af74c5c7c923157
SaraMur <saramurcomercial@gmail.com>
2022-02-06 08:42:21 -0300

    funcionalidad obtener mas data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const paginaSiguiente = document.querySelector("#next")
 
 let paginaPresente = 1
 
+//Funcion que se ejecuta al cargar la pagina
 const getDataCharacters = () =>{
     fetch(`https:rickandmortyapi.com/api/character?page=${paginaPresente}`)
     .then((res) => res.json())
@@ -37,14 +38,22 @@ const obtenerDataUbicacion = () =>{
 //OBTENER DATA AL CARGAR LA PAGINA
 
 getDataCharacters()
+const getCharacterInfo = (id) => {
+    const divCardCharacterInfoHidden = document.querySelector("#char_" + `${curr.id}`)
+    divCardCharacterInfoHidden.classList.toggle('div-block');
+}
 
 const cardsCharacters = (data) => {
-
     const html = data.reduce((acc, curr) => {
        return acc + `
-        <article class = "cards-characters">
-        <img src="${curr.image}">
-        <h2>${curr.name}</h2>
+        <article class="cards-characters" onclick="getCharacterInfo(${curr.id})">
+            <img src="${curr.image}">
+            <h2>${curr.name}</h2>
+            <div id="char_'${curr.id}'" class="div-hidden">
+                <h3 class="curr-info">${curr.gender}</h3>
+                <h3 class="curr-info">${curr.origin}</h3>
+                <h3 class="curr-info">${curr.status}</h3>
+            </div>
         </article>
         `
     }, "")
@@ -56,7 +65,9 @@ const cardsCharacters = (data) => {
 const form = document.querySelector("#form")
 const inputName = document.querySelector("#input")
 const inputSpecies = document.querySelector("#inputSpecies")
+const sortBy = document.querySelector("#sort-by")
 
+//Funcion que se ejecuta al clickear buscar
 const buscarPersonajes = () => {
     const name = inputName.value
     const species = inputSpecies.value
@@ -73,9 +84,40 @@ const buscarPersonajes = () => {
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        cardsCharacters(data.results)
+        const personajes = data.results;
+        //Ordenar personajes por Deafault A-Z
+        debugger
+        if(sortBy.value==='az'){
+            personajes.sort(function (a, b) {
+                if (a.name > b.name) {
+                  return 1;
+                }
+                if (a.name < b.name) {
+                  return -1;
+                }
+                // a must be equal to b
+                return 0;
+              });
+        }
+        if(sortBy.value==='za'){
+            personajes.sort(function (a, b) {
+                if (a.name < b.name) {
+                  return 1;
+                }
+                if (a.name > b.name) {
+                  return -1;
+                }
+                // a must be equal to b
+                return 0;
+              });
+        }
+
+
+
+        cardsCharacters(personajes)
     })
 }
+
 
 
 form.onsubmit = (e) =>{
@@ -94,128 +136,3 @@ paginaPrevia.onclick = () => {
     paginaPresente = paginaPresente - 1
     getDataCharacters()
 }
-
-
-/**************************************************************************** */
-//BOTON BUSCAR
-/* botonBuscar.onclick = () =>{
-    botonBuscar.classList.add("is-loading")
-}; */
-
-
-
-/************************************************************************** */
-//EPISODIOS
-/* obtenerDataEpisodios()
-
-const cardsEpisodios = (data) => {
-
-    const html = data.reduce((acc, curr) => {
-       return acc + `
-        <article class = "cards-episodios">
-        <img src="${curr.image}">
-        <h2>${curr.name}</h2>
-        </article>
-        `
-    }, "")
-    rootCard.innerHTML = html 
-}
-
-//CONTROLES DE PAGINACION
-
-paginaSiguiente.onclick = () => {
-    paginaPresente = paginaPresente + 1
-    obtenerDataEpisodios()
-}
-
-paginaPrevia.onclick = () => {
-    paginaPresente = paginaPresente - 1
-    obtenerDataEpisodios()
-}
- */
-
-/*************************************************************************** */
-//UBICACION
-/* obtenerDataUbicacion()
-
-const cardsUbicacion = (data) => {
-
-    const html = data.reduce((acc, curr) => {
-       return acc + `
-        <article class = "cards-ubicacion">
-        <img src="${curr.image}">
-        <h2>${curr.name}</h2>
-        </article>
-        `
-    }, "")
-    rootCard.innerHTML = html 
-}
- */
-//CONTROLES DE PAGINACION
-
-/* paginaSiguiente.onclick = () => {
-    paginaPresente = paginaPresente + 1
-    obtenerDataUbicacion()
-}
-
-paginaPrevia.onclick = () => {
-    paginaPresente = paginaPresente - 1
-    obtenerDataUbicacion()
-}
- */
-/*************************************************************************** */
-//MOSTRAR / OCULTAR CARDS
-
-/* if (azPersonajes = checked) {
-    cardsCharacters.style.display = 'none';
-}else{
-    cardsCharacters.style.display = 'block';
-}
- */
-
-/*************************************************************************** */
-
-
-/* const cargarCombo = () => {
-
-    let ordenA = document.createElement("option");
-    let ordenZ = document.createElement("option");
-    let ordenNew = document.createElement("option");
-    let ordenOld = document.createElement("option");
-
-
-    let textA = document.createTextNode("A/Z");
-    let textZ = document.createTextNode("Z/A");
-    let textNew = document.createTextNode("Más Nuevos");
-    let textOld = document.createTextNode("Más Viejos");
-
-    ordenA.appendChild(textA);
-    ordenZ.appendChild(textZ);
-    ordenNew.appendChild(textNew);
-    ordenOld.appendChild(textOld);
-
-    orden.innerHTML = '';
-
-    orden.appendChild(ordenA);
-    orden.appendChild(ordenZ);
-    if (tipo.value === 'personajes') {
-        orden.appendChild(ordenNew);
-        orden.appendChild(ordenOld);
-    }
-
-} */
-
-
-
-//INPUT URL DE IMAGEN
-/* 
-const inputBusqueda = document.getElementById('input');
-
-
-inputBusqueda.addEventListener('keyup', (e) => {
-    const src = urlImg.value;
-    previewImagePanel.style.backgroundImage = `url('${src}')`;
-    previewImagePanel.style.backgroundPosition = 'center';
-    previewImagePanel.style.backgroundRepeat = 'no-repeat';
-    previewImagePanel.style.backgroundSize = 'cover';
-}); */
